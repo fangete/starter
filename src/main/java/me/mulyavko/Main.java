0726fb9c032f4cb45553014185b29be6b1dbaba5
ILYA MULYAVKO <ilya.mulyavko@gmail.com>
2023-05-17 15:59:20 +0300

    create package + biuld options in pom

--- src/main/java/me/mulyavko/Main.java
+++ src/main/java/me/mulyavko/Main.java
@@ -1,4 +1,6 @@
-import bot.TelegramBot;
+package me.mulyavko;
+
+import me.mulyavko.bot.TelegramBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
--- src/main/java/me/mulyavko/bot/TelegramBot.java
+++ src/main/java/me/mulyavko/bot/TelegramBot.java
@@ -1,4 +1,4 @@
-package bot;
+package me.mulyavko.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
