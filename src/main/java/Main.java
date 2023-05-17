8c4db076aa33749f3c5bfafa4f92319990868b26
ILYA MULYAVKO <ilya.mulyavko@gmail.com>
2023-05-17 14:31:29 +0300

    Initial Starter Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import bot.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi = null;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/main/java/bot/TelegramBot.java
@@ -0,0 +1,35 @@
+package bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        SendMessage sendMessage = new SendMessage();
+
+        String chatID = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
+
+
+        sendMessage.setChatId(chatID);
+        sendMessage.setText("echo: " + text);
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "IL_starter_bot";
+    }
+    @Override
+    public String getBotToken() {
+        return "5901000381:AAHc0t_L-JRzjvW7eVEc-WfQMk2AErrPhq8";
+    }
+}
